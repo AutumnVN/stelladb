2e0ac4bc0763fc9204e6f75ed2d0733cb11f676b
AutumnVN <autumnvnchino@gmail.com>
2026-01-03 14:31:57 +0700

    color scheme only dark

--- src/scripts/infodoc.js
+++ src/scripts/infodoc.js
@@ -9,34 +9,26 @@ export const style = `
         .docs-butterbar-container {
             display: none !important;
         }
+
+        :root {
+            color-scheme: only dark;
+        }
+
         body {
             overflow: auto;
             background: #303030 !important;
         }
+
         [style*='overflow:'][style*='height:'][style*='width:'] {
             overflow: visible !important;
             height: auto !important;
             width: auto !important;
         }
+
         #waffle-grid-container {
             display: inline-block
         }
-    </style>
-`;
 
-export const nav = `
-    <nav>
-        <a href="/" style="color: #6f9; font-weight: 700;">stelladb</a>
-        <a href="https://docs.google.com/spreadsheets/d/1otsS2C1RkXLaFSvp2SMOS-vtRBaEBpZlcgR361_fdAE/edit?gid=1265175955#gid=1265175955" style="color: #ccf; font-weight: 700;">original infodoc</a>
-        <a href="/infodoc">build</a>
-        <a href="/infodoc/aqua">aqua</a>
-        <a href="/infodoc/ignis">ignis</a>
-        <a href="/infodoc/terra">terra</a>
-        <a href="/infodoc/ventus">ventus</a>
-        <a href="/infodoc/lux">lux</a>
-        <a href="/infodoc/umbra">umbra</a>
-    </nav>
-    <style>
         nav {
             display: inline-flex;
             position: sticky;
@@ -53,6 +45,7 @@ export const nav = `
             line-height: 1.3;
             min-width: 100vw;
         }
+
         nav>a {
             padding: 0.5rem;
             font-size: 1rem;
@@ -60,10 +53,25 @@ export const nav = `
             color: #bbb;
             text-decoration: none;
         }
+
         nav>a:hover {
             color: #eee;
         }
     </style>
+`;
+
+export const nav = `
+    <nav>
+        <a href="/" style="color: #6f9; font-weight: 700;">stelladb</a>
+        <a href="https://docs.google.com/spreadsheets/d/1otsS2C1RkXLaFSvp2SMOS-vtRBaEBpZlcgR361_fdAE/edit?gid=1265175955#gid=1265175955" style="color: #ccf; font-weight: 700;">original infodoc</a>
+        <a href="/infodoc">build</a>
+        <a href="/infodoc/aqua">aqua</a>
+        <a href="/infodoc/ignis">ignis</a>
+        <a href="/infodoc/terra">terra</a>
+        <a href="/infodoc/ventus">ventus</a>
+        <a href="/infodoc/lux">lux</a>
+        <a href="/infodoc/umbra">umbra</a>
+    </nav>
 `;
 
 export function clientScript(url) {

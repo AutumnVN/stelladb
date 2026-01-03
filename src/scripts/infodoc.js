a96690f004cdc87966989c4a7fb74148d546a7aa
AutumnVN <autumnvnchino@gmail.com>
2026-01-03 17:44:04 +0700

    infodoc v2

--- src/scripts/infodoc.js
+++ src/scripts/infodoc.js
@@ -1,13 +1,15 @@
 export const style = `
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.5, minimum-scale=0.5">
     <style>
-        #docs-chrome,
-        #grid-bottom-bar,
-        #core-js-error-dialog-overlay,
         .column-headers-background,
-        .row-headers-background,
-        .docs-butterbar-container {
+        .row-headers-background {
             display: none !important;
+        }
+
+        .grid-container {
+            overflow: unset !important;
+            background: none !important;
         }
 
         :root {
@@ -15,35 +17,23 @@ export const style = `
         }
 
         body {
-            overflow: auto;
+            margin: 0;
             background: #303030 !important;
         }
 
-        [style*='overflow:'][style*='height:'][style*='width:'] {
-            overflow: visible !important;
-            height: auto !important;
-            width: auto !important;
-        }
-
-        #waffle-grid-container {
-            display: inline-block
-        }
-
         nav {
-            display: inline-flex;
+            display: flex;
             position: sticky;
             top: 0;
             left: 0;
             align-items: center;
             z-index: 727;
             backdrop-filter: blur(5px);
-            margin: 0 auto;
             padding: 0 1rem;
             background: #30303066;
             white-space: nowrap;
             gap: 0.5rem;
             line-height: 1.3;
-            min-width: 100vw;
         }
 
         nav>a {
@@ -63,7 +53,7 @@ export const style = `
 export const nav = `
     <nav>
         <a href="/" style="color: #6f9; font-weight: 700;">stelladb</a>
-        <a href="https://docs.google.com/spreadsheets/d/1otsS2C1RkXLaFSvp2SMOS-vtRBaEBpZlcgR361_fdAE/edit?gid=1265175955#gid=1265175955" style="color: #ccf; font-weight: 700;">original infodoc</a>
+        <a href="https://docs.google.com/spreadsheets/d/1otsS2C1RkXLaFSvp2SMOS-vtRBaEBpZlcgR361_fdAE/edit?gid=1265175955#\w+?=1265175955" style="color: #ccf; font-weight: 700;">original infodoc</a>
         <a href="/infodoc">build</a>
         <a href="/infodoc/aqua">aqua</a>
         <a href="/infodoc/ignis">ignis</a>
@@ -78,18 +68,18 @@ export function clientScript(url) {
     return `
     <script>
         (async () => {
-            const url = ${JSON.stringify(url)};
+            const url = '${url}';
             try {
-                const res = await fetch(url,
-                    {
-                        headers: {
-                            'sec-ch-ua-mobile': '?0'
-                        }
-                    }
-                });
+                const res = await fetch(url);
                 const html = await res.text();
                 const modifiedHtml = html
-                    .replace('<div class="grid-table-container">', \`${nav}$&\`)
+                    .replace(/<meta name="viewport".+?>/, '')
+                    .replace(/style="display:none;position:relative;"/, '')
+                    .replace(/<link href='\\/static.+?>/g, '')
+                    .replace(/<style>@import.+?<\\/style>/, '')
+                    .replace(/target="_blank" rel="noreferrer" href="#\\w+?=\\d+?"/g, 'href="javascript:void(0);"')
+                    .replace(/<img src="https:\\/\\/lh7-rt\\.googleusercontent\\.com/g, '<img referrerpolicy="no-referrer" src="https://lh7-rt.googleusercontent.com')
+                    .replace(/<div id="\\d+?".+?>/, \`${nav}$&\`)
                     .replace(/<head>/, \`$&${style}\`);
                 document.documentElement.innerHTML = modifiedHtml;
             } catch (e) {
